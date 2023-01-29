a93f4391f5578886f08083a5875a78e532e91937
AnBoyvan <andrew.boyvan1988@gmail.com>
2023-01-29 10:29:11 +0200

    fix form error

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,34 @@
 import PropTypes from 'prop-types';
-import { Formik, Field } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import css from './Filter.module.css';
 
 export const Filter = ({ value, onChange }) => {
+  const initialValues = {
+    filter: '',
+  };
   const handleChange = e => {
     onChange(e.currentTarget.value);
+  };
+
+  const handleSubmit = ({ filter }, { resetForm }) => {
+    onChange(filter);
+    resetForm();
   };
 
   return (
     <>
       <p className={css.title}>Find contacts by name</p>
-      <Formik>
-        <Field
-          className={css.input}
-          type="text"
-          name="filter"
-          value={value}
-          onChange={handleChange}
-        ></Field>
+
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <Field
+            className={css.input}
+            type="text"
+            name="filter"
+            value={value}
+            onChange={handleChange}
+          ></Field>
+        </Form>
       </Formik>
     </>
   );
