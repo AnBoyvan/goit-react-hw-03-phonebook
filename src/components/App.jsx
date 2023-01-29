a51ea78bc87f380538c03c46f7c6dc94e6b1b25f
AnBoyvan <andrew.boyvan1988@gmail.com>
2023-01-29 14:17:16 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,6 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import css from './App.module.css';
 
-// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-// { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-// { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-// { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 export class App extends Component {
   state = {
     contacts: [
@@ -24,6 +20,13 @@ export class App extends Component {
     const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
     }
   }
 
@@ -65,13 +68,6 @@ export class App extends Component {
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
-
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
 
   render() {
     const { filter, contacts } = this.state;
